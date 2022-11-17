c4daa045a9d2dcb81692a0589969c1d921794473
Alyona Teyber <Ivanova.alyona5@gmail.com>
2022-11-17 11:17:32 -0800

    Update openfido.sh

--- openfido.sh
+++ openfido.sh
@@ -71,6 +71,8 @@ if [ "$ANALYSIS" = "vegetation_analysis" ]; then
     gridlabd geodata merge -D elevation $OPENFIDO_INPUT/$POLE_DATA -r 30 | gridlabd geodata merge -D vegetation >$OPENFIDO_OUTPUT/path_vege.csv
     python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/add_info.py # this needs to get integrated into the gridlabd source code
     gridlabd geodata merge -D powerline $OPENFIDO_OUTPUT/path_vege.csv --cable_type="TACSR/AC 610mm^2" >$OPENFIDO_OUTPUT/path_result.csv
+    python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/folium_data.py
+    gridlabd /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/folium.glm -D html_save_options="--cluster" -o $OPENFIDO_OUTPUT/folium.html
 elif ["$ANALYSIS"="pole analysis"]; then 
     echo "PENDING POLE ANALYSIS"
 fi 
